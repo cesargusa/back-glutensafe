b288af43e8c11c0a23043bc04d4b3d0a7cd44ab8
cesargusa <cesargusa22@gmail.com>
2024-12-20 00:19:52 +0100

    filtros añadidos

--- api/routes/companyRoutes.js
+++ api/routes/companyRoutes.js
@@ -5,39 +5,30 @@ const router = Router();
 const SECRET_KEY = process.env.SUPABASE_KEY;
 
 router.get('/', async (req, res) => {
-  // Validación del token de autorización
-  // const token = req.headers.authorization?.split(' ')[1];
-  // if (token && token === process.env.TOKEN && (process.env.ROLE === 'ADMIN' || process.env.ROLE === 'SUPER_ADMIN')) {
-    try {
-      let data, error;
+  try {
+    const { name, city } = req.query; 
+    let query = supabase.from('dbo.Company').select('*');
 
-      // Consulta diferente según el rol del usuario
-      // if (process.env.ROLE === 'SUPER_ADMIN') {
-      //   ({ data, error } = await supabase.from('dbo.bond').select('*'));
-      // } else if (process.env.ROLE === 'ADMIN' && process.env.USER_ID) {
-      //   // Para rol ADMIN, solo consulta registros que coincidan con `userCreatedId`
-      //   ({ data, error } = await supabase.from('dbo.bond').select('*').eq('userCreatedId', process.env.USER_ID));
-      // } else {
-        // Si USER_ID no está definido para el rol ADMIN
-       ({ data, error } = await supabase.from('dbo.Company').select('*'));
-      // }
-
-      // Manejo de errores de la consulta
-      if (error) {
-        console.error("Error en la consulta a Supabase:", error);
-        return res.status(400).json({ error: error.message });
-      }
-
-      // Respuesta exitosa
-      return res.status(200).json(data);
-    } catch (err) {
-      console.error("Error inesperado:", err);
-      return res.status(500).json({ error: "Error al procesar la solicitud" });
+    if (name) {
+      query = query.ilike('name', `%${name}%`); 
     }
-  // } else {
-  //   // Error de autenticación
-  //   return res.status(401).json({ message: "Error de token" });
-  // }
+
+    if (city) {
+      query = query.ilike('city', `%${city}%`); 
+    }
+
+    const { data, error } = await query;
+
+    if (error) {
+      console.error("Error en la consulta a Supabase:", error);
+      return res.status(400).json({ error: error.message });
+    }
+
+    return res.status(200).json(data);
+  } catch (err) {
+    console.error("Error inesperado:", err);
+    return res.status(500).json({ error: "Error al procesar la solicitud" });
+  }
 });
 
 export default router;
